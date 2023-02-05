bbd821201eda2808e094d60a2a9034f8896cbd72
ibrahim karataş <ibrahimkaratas@windowslive.com>
2023-02-05 17:03:52 +0300

    ML

---
+++ android/app/src/main/kotlin/com/example/catanddogpredictor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.catanddogpredictor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/splashscreen.dart
@@ -0,0 +1,32 @@
+import 'package:catanddogpredictor/home.dart';
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class MySplash extends StatefulWidget {
+  @override
+  _MySplashState createState() => _MySplashState();
+}
+
+class _MySplashState extends State<MySplash> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 2,
+      navigateAfterSeconds: Home(),
+      title: Text(
+        'Dog and Cat',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 30,
+          color: Color(
+            0xFFE99600,
+          ),
+        ),
+      ),
+      image: Image.asset('assets/cat.png'),
+      photoSize: 50.0,
+      backgroundColor: Colors.black,
+      loaderColor: Color(0xFFEEDA28),
+    );
+  }
+}
